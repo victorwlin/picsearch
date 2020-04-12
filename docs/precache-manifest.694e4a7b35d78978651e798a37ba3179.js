f6771b915f13037cfc502d28a3dba9576d207196
victorwlin <57220247+victorwlin@users.noreply.github.com>
2020-04-12 19:44:07 +0800

    Add files via upload

---
+++ docs/precache-manifest.694e4a7b35d78978651e798a37ba3179.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "748ff0831a534856507c37b7dc154d4c",
+    "url": "/picsearch/index.html"
+  },
+  {
+    "revision": "f7b6903e5b5288a2dd17",
+    "url": "/picsearch/static/css/main.c00f04db.chunk.css"
+  },
+  {
+    "revision": "2d4bd92103f9d0f3bcaf",
+    "url": "/picsearch/static/js/2.c4df6d31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/picsearch/static/js/2.c4df6d31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7b6903e5b5288a2dd17",
+    "url": "/picsearch/static/js/main.b8c909ac.chunk.js"
+  },
+  {
+    "revision": "f5dbd4e64e84238dccfe",
+    "url": "/picsearch/static/js/runtime-main.1ceae47a.js"
+  }
+]);
